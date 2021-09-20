d34f68230286a2cd0a8982a3d3f1f3e4d8e9615d
aeyonblack <tanyamasvitaog@gmail.com>
2021-09-20 15:01:22 +0200

    query all countries from REST API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { MenuItem, FormControl, Select } from '@mui/material';
+
+const countries_url = 'https://disease.sh/v3/covid-19/countries';
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'South Africa', 'China']);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      await fetch(countries_url)
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => {
+            return {
+              name: country.country,
+              value: country.countryInfo.iso2,
+            };
+          });
+          setCountries(countries);
+        });
+    };
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
